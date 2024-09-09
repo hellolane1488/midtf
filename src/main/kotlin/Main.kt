2269c42ea14ecd9c88f83f703d8dcab82a5728e3
hellolane1488 <lane@dvxchsquad.dev>
2024-09-10 02:10:58 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 fun main(args: Array<String>) {
     if (System.getProperty("os.name").lowercase(Locale.getDefault()) != "linux") {
-        println("Sorry, but the script does not work correctly on your system")
+        println("Sorry, but the script does not work correctly on your system. ИДИ НАХУЙ ХУИСОС")
         /* So far, this script only works correctly on Linux distributions */
     }
 
